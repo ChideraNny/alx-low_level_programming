92175b91adc2cbb5843542d38ae5a01922124083
ChideraNny <chiderannonyelu@gmail.com>
2022-04-10 11:31:06 -0700

    lastdigit1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there*/
+/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main function
+ * Description: task 2
+ * Return: 0 always (Success)
  */
 int main(void)
 {
-		int n, s;
-
-
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		/* your code goes there */
-		s = n % 10;
-		if (s > 5)
-		{
-		printf("Last digit of %d is %d and is greater than 5\n", n, s);
-		}
-	<<<<<<< HEAD
-		if (num == 0)
-	=======
-		else if (s == 0)
-	>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-		{
-		printf("Last digit of %d is %d and is 0\n", n, s);
-		}
-	<<<<<<< HEAD
-		if (num < 6)
-	=======
-		else if (s < 6)
-	>>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-		{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-		}
-		return (0);
+int n;
+int last;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes here */
+last = n % 10;
+if (last == 0)
+{
+printf("Last digit of %i is %i and is 0\n". n, last);
+}
+else if (last > 5)
+{
+printf("Last digit of %i is %i and is greater than 5\n", n, last);
+}
+else if (last < 6 && last != 0)
+{
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+}
+return (0);
 }
