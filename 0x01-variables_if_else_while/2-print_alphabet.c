08cd77588c2a7db9bb82e7bbfcd9db8fbd94bbe8
ChideraNny <chiderannonyelu@gmail.com>
2022-04-11 14:11:15 -0700

    alphabetp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: task 2
+ * Return: 0
  */
 int main(void)
 {
-<<<<<<< HEAD
-int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-=======
-int p;
-p = 97;
-while (p <= 122)
+char c = 'a';
+while (c <= 'z')
 {
-putchar(p);
-p++;
+putchar(c);
+c++;
 }
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
 putchar('\n');
 return (0);
 }
