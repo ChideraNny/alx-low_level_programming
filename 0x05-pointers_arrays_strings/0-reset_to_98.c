01d656290f410ad3951e2fe74954898f8aa466b7
ChideraNny <chiderannonyelu@gmail.com>
2022-04-21 06:01:41 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * reset_to_98 - main
- * @n: - pinter to int
-*/
-
+ * reset_to_98 - Entry function
+ * @n: pointer to in variable type
+ * Return: nothing
+ */
 void reset_to_98(int *n)
 {
 *n = 98;
